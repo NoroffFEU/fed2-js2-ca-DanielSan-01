1d58e02a00d53a9496dc9794f5bc7ca9fcc308d3
Daniel Østensen <98233180+DanielSan-01@users.noreply.github.com>
2024-09-20 21:07:26 +0200

    innit build of CRUD site
    Added api functionality for token, login and register.

--- src/js/api/auth/login.js
+++ src/js/api/auth/login.js
@@ -1,1 +1,39 @@
-export async function login({ email, password }) {}
+//import  { API_BASE_URL } from "../../../config"
+import { getKey } from './key.js'
+
+export async function login(event) {
+  event.preventDefault();
+  const email = document.querySelector("#email").value
+  const password = document.querySelector("#password").value
+  console.log(email);
+
+  const response = await fetch(`https://v2.api.noroff.dev/auth/login`, {
+    method: 'POST',
+    headers : {
+      'Content-Type': 'application/json'
+    }, 
+    body: JSON.stringify({email, password})
+  });
+
+  const loginData = await response.json();
+  console.log(loginData.data);
+
+  if (response.ok) {
+    localStorage.setItem('token', loginData.data.accessToken);
+    console.log(loginData.data.accessToken);
+    await getKey();
+    console.log('Login successful', loginData);
+
+  } else {
+    console.log('Login Failed', loginData);
+  }
+
+}
+
+document.querySelector('form[name="login"]').addEventListener('submit', login);
+
+
+//recive login info
+// fetch api loginurl
+//https://v2.api.noroff.dev//auth/create-api-key
+// request method POST and request in JSON format
